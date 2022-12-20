42c00783ce3f9cdb77b9495df5f850b458694e9d
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-20 19:01:43 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+
+const COLOR_SWITCHER_DELAY = 1000;
+let intervalId = null;
+refs.buttonStop.disabled = true;
+
+refs.buttonStart.addEventListener('click', onButtonClickStartSwitcher);
+refs.buttonStop.addEventListener('click', onButtonClickStopSwitcher);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClickStartSwitcher() {
+  intervalId = setInterval(colorSwitcher, COLOR_SWITCHER_DELAY);
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+
+  console.log('start');
+}
+
+function onButtonClickStopSwitcher() {
+  clearInterval(intervalId);
+  refs.buttonStart.disabled = false;
+  refs.buttonStop.disabled = true;
+  console.log('Stop');
+}
+
+function colorSwitcher() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  console.log(`Current color HEX ${randomColor}`);
+}
+
 
